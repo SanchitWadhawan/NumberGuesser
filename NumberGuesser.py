bb1bc6d3731f6b8958c5b0514d07af0f6a99a26e
Sanchit Wadhawan <83018579+SanchitWadhawan@users.noreply.github.com>
2022-04-02 13:14:54 +0530

    Add files via upload

---
+++ NumberGuesser.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+## Guess the number by computer
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess=0
+    while guess != random_number:
+        guess =int(input("Enter a number between 1 and x : "))
+        if guess < random_number :
+            print("Sorry , guess again, too low ")
+        elif guess > random_number :
+            print("Sorry , too high")
+    print("Yayy , Congrats ,You got it")
+    
+    
+        
+guess(10)
+
+
+# In[ ]:
+
+
+
+
+
+# In[23]:
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
+
+guess(10)
+
+
+# In[ ]:
+
+
+
+
